aa8f5d76b48d8e115189938302af0515e46c6ec7
joshuanelsondev <joshuanelsondev@gmail.com>
2023-01-23 11:30:34 -0500

    Edit media queries

--- gameRoom.js
+++ gameRoom.js
@@ -249,7 +249,7 @@ function clearScores() {
 }
 
 function createShuffleButton()  {
-    if (remainingCardCount <= 10) {
+    if (remainingCardCount <= 20) {
         dealBtn.textContent = "Shuffle Deck";
     }
 }
